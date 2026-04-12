76dc66666cdc354c00345854ca488e64537235c6
Vadim Geczko <foxia722@gmail.com>
2026-04-13 00:26:26 +0300

    Add files via upload

--- 4.3python.py
+++ 4.3python.py
@@ -1,23 +1,22 @@
-from math import factorial as fct
 try:
     p = float(input(("Введите значение p (Число которое должно быть меньше модуля числа на числовом ряду): ")))
 except (ValueError, TypeError):
     print("Неправильный ввод числа p!")
     
+def fct(n):
+    factorial = 1
+    for i in range(1, n+1):
+        factorial *= i
+    return factorial
+
 total_sum = 0.0
-n = 0 # начало ряда (первый член ряда)
 max_iterations = int(input(("Введите максимальное число итераций для вычисления суммы: ")))
 # Счетчик итераций для защиты от бесконечного цикла
-iteration_count = 0
-a = 0
-while iteration_count < max_iterations:
+for n in range(1, max_iterations+1):
+    a = 10**n / fct(n)
+    n += 1
     if abs(a) >= p:
         total_sum += a
-    a = 10**n / fct(n)
-    # Переход к следующему члену ряда
-    n += 1
-    iteration_count += 1
-    total_sum += a
 # Проверка на достижение максимального количества итераций
 print(f"Предупреждение: достигнут лимит итераций ({max_iterations}).")
-print(f"Ваша сумма членов числового ряда: {total_sum}")
+print(f"Ваша сумма членов числового ряда: {total_sum:.3f}")
--- 4.4python.py
+++ 4.4python.py
@@ -1,4 +1,6 @@
 from math import *
+from tabulate import tabulate
+
 
 try:
      a = float(input("Введите начало a: "))
@@ -11,25 +13,31 @@ if (a > b and h > 0) or h == 0:
        print("Неккоректное значение h!")
        exit()
 
-print("x\t\tF(x)")
+table = []
 
 if a > b:
        x = b
-       while x <= a:
+       while x < a:
         if sin(x) == 0: ## ctg = cos/sin
-             print(f"{x:.3f}\t\tне определена")
-             x -= h
+               table_val = "не определена"
+               x -= h
+               table.append([round(x, 3), table_val])
         else:
-              result = cos(x) / sin(x) + 1
-              print(f"{x:.3f}\t\t{result:.3f}")
-              x -= h
+               result = cos(x) / sin(x) + 1
+               table_val = round(cos(x) / sin(x) + 1, 3)
+               x -= h
+               table.append([round(x, 3), table_val])
 if a < b:
        x = a
-       while x <= b:
+       while x < b:
         if sin(x) == 0: ## ctg = cos/sin
-             print(f"{x:.3f}\t\tне определена")
-             x += h
+               table_val = "не определена"
+               x += h
+               table.append([round(x, 3), table_val])
         else:
-              result = cos(x) / sin(x) + 1
-              print(f"{x:.3f}\t\t{result:.3f}")
-              x += h
+               result = cos(x) / sin(x) + 1
+               table_val = round(cos(x) / sin(x) + 1, 3)
+               x += h
+               table.append([round(x, 3), table_val])
+
+print(tabulate(table, headers=["x", "f(x)"], tablefmt="grid"))
